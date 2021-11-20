6f550e5bb295bfc10cd1b2687dcdbc2a55587d6c
Nathan Irvin <65375604+irv0735@users.noreply.github.com>
2021-11-20 07:30:41 -0700

    fixed typo in typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -39,7 +39,7 @@ const typeDefs = gql`
   }
 
   type Mutation {
-    createUser(username: String!, email: String!, password, String!): Auth
+    createUser(username: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
 
     saveBook(input: bookInput): User
