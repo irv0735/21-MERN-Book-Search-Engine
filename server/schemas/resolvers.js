2a77969a1ffe43d6874e8f46bc04c7e0866d08df
Nathan Irvin <65375604+irv0735@users.noreply.github.com>
2021-11-20 13:10:57 -0700

    fixed error in signupform

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -12,8 +12,8 @@ const resolvers = {
   },
 
   Mutation: {
-    addUser: async (parent, args) => {
-      const user = await User.create(args);
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
       if (!user) {
         throw new Error('Unable to create new User');
       }
